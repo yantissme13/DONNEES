19c27a4ad41e3ed450771462979c52fd9557e4c7
yantissme13 <sognonyanis@gmail.com>
2025-02-26 22:45:33 +0100

    Message de modification

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.get('/odds/filter', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Serveur lancé sur le port ${PORT}`));
 
